6bc444fae7e7995370746e19f4d884c8e76f8ae1
bgieszl <91099403+bgieszl@users.noreply.github.com>
2021-11-08 16:30:50 -0800

    Iteration 3

--- connect.php
+++ connect.php
@@ -2,6 +2,7 @@
 	$realname = $_POST['realname'];
 	$email = $_POST['email'];
 	$pass = $_POST['pass'];
+	$hashedpw = password_hash($pass, PASSWORD_DEFAULT);
 	$username = $_POST['username'];
 
 	//Database connection
@@ -19,18 +20,22 @@
 		
 		if(mysqli_num_rows($dup) > 0){
 			echo "This email is already taken ";
+			header('Location: signup.html');
 		}else if(mysqli_num_rows($dup2) > 0){
 			echo "This username is already taken ";
+			header('Location: signup.html');
 		}else{
 			
 		$INSERT = "INSERT INTO userprofile (realname, email, pass, username) 
 			values(?,?,?,?)";
 		$stmt = $conn->prepare($INSERT);
-		$stmt->bind_param("ssss", $realname, $email, $pass, $username);
+		$stmt->bind_param("ssss", $realname, $email, $hashedpw, $username);
 		$stmt->execute();
 		echo "registration successfully...";
+		
 		$stmt->close();
 		$conn->close();
+		header('Location: result.html');
 		}
 		
 	}
