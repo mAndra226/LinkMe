15211c2aab88294bc23268a6052a9217c70bafb4
bgieszl <91099403+bgieszl@users.noreply.github.com>
2021-11-16 12:48:53 -0800

    Iteration 4
    updates the database and user upload profile pics

--- editprofile.php
+++ editprofile.php
@@ -21,12 +21,51 @@ $dbUsername = "root";
 $dbPassword = "";
 $dbName = "linkme_database";
 $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+$ID = $_SESSION['uid'];
 if(mysqli_connect_error()){
 	die('Connection Failed: '.mysqli_connect_error());
 }else{
-		$INSERT = "INSERT INTO userprofile (realname, email, country, phone, bio) 
-		values(?,?,?,?)";
-		$stmt = $conn->prepare($INSERT);
-		$stmt->bind_param("ssss", $realname, $email, $country, $phone, $bio);
-		$stmt->execute();	
+		//profile pic
+		//if (isset($_POST['Save'])){
+			$file = $_FILES['file'];
+			
+			$fileName = $_FILES['file']['name'];
+			$fileTmpName = $_FILES['file']['tmp_name'];
+			$fileSize = $_FILES['file']['size'];
+			$fileError = $_FILES['file']['error'];
+			$fileType = $_FILES['file']['type'];
+			
+			$fileExt = explode('.', $fileName);
+			$fileActualExt = strtolower(end($fileExt));
+			
+			$allowed = array('jpg', 'jpeg', 'png');
+			
+			if(in_array($fileActualExt, $allowed)){
+				if($fileError === 0){
+					if($fileSize < 1000000){
+						$fileNameNew = uniqid('', true).".".$fileActualExt;
+						$fileDestination = 'uploads/'.$fileNameNew;
+						move_uploaded_file($fileTmpName, $fileDestination);
+					}else{
+						echo "File size too big";
+					}
+				}else{
+					echo "File upload error";
+				}
+			}else{
+				echo "File type not allowed";
+			}
+		//}
+		
+		
+		
+		
+		//add basic info
+		$UPDATE = "UPDATE userprofile SET realname = ?, email = ?, country = ?, phone = ?, bio = ?, image = ?
+		WHERE username = ?";
+		$stmt = $conn->prepare($UPDATE);
+		$stmt->bind_param('sssssss', $realname, $email, $address, $phone, $bio, $fileDestination, $_SESSION['uid']);
+		$stmt->execute();
+		header("Location: result.html?ID=$ID");
+		exit();		
 }
