dcb8c50a4b07f94b460c231ef346d8234c39c2f4
bgieszl <91099403+bgieszl@users.noreply.github.com>
2021-12-07 10:25:39 -0800

    Iteration 4
    changes so links and edit php work

--- linkmeDatabase.sql
+++ linkmeDatabase.sql
@@ -11,7 +11,6 @@ USE linkme_database;
 -- Creating the UserProfile Table:
 DROP TABLE IF EXISTS UserProfile;
 CREATE TABLE UserProfile (
-	Userid				int(11)	 		AUTO_INCREMENT	NOT NULL,
     realname			VARCHAR(300) 	NOT NULL,
 	email				VARCHAR(300) 	NOT NULL,
     pass				VARCHAR(300) 	NOT NULL,
@@ -20,7 +19,7 @@ CREATE TABLE UserProfile (
     phone				VARCHAR(15) 	,
     bio					VARCHAR(500)	,
     image				VARCHAR(300)	,
-    PRIMARY KEY (Userid)
+    PRIMARY KEY (username)
    
 );
 
@@ -28,92 +27,92 @@ CREATE TABLE UserProfile (
 DROP TABLE IF EXISTS Facebook;
 CREATE TABLE Facebook (
 	FBLINK				VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 	NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (FBLINK),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
 
 -- Creating the Twitter Table:
 DROP TABLE IF EXISTS Twitter;
 CREATE TABLE Twitter (
 	twitterlink			VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 	NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (twitterlink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
     
 -- Creating the Instagram Table:
 DROP TABLE IF EXISTS Instagram;
 CREATE TABLE Instagram (
 	instalink			VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300) 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (instalink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
     
 -- Creating the Linkedin Table:
 DROP TABLE IF EXISTS Linkedin;
 CREATE TABLE Linkedin (
 	linkedinlink		VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (linkedinlink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
     
 -- Creating the Github Table:
 DROP TABLE IF EXISTS Github;
 CREATE TABLE Github (
 	githublink			VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (githublink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
 -- Creating the Youtube Table:
 DROP TABLE IF EXISTS Youtube;
 CREATE TABLE Youtube (
 	ytlink				VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300) 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (ytlink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
 -- Creating the Snapchat Table:
 DROP TABLE IF EXISTS Snapchat;
 CREATE TABLE Snapchat (
 	sclink			VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (sclink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
 -- Creating the Reddit Table:
 DROP TABLE IF EXISTS Reddit;
 CREATE TABLE Reddit (
 	redditlink			VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (redditlink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
 -- Creating the Pinterest Table:
 DROP TABLE IF EXISTS Pinterest;
 CREATE TABLE Pinterest (
 	pintlink			VARCHAR(300)		NOT NULL,
-    Userid				int(11)	 			NOT NULL,
+    Userid				VARCHAR(300)	 			NOT NULL,
 	clickcount			int(11)				NOT NULL,
     PRIMARY KEY (pintlink),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
     
 -- Creating the Links Table:
 DROP TABLE IF EXISTS Links;
 CREATE TABLE Links (
-	Userid				int(11)	 			NOT NULL,
+	Userid				VARCHAR(300)	 			NOT NULL,
     Facebook			VARCHAR(300),
     Twitter				VARCHAR(300),
 	Instagram			VARCHAR(300),
@@ -123,14 +122,5 @@ CREATE TABLE Links (
     Snapchat			VARCHAR(300),
     Reddit				VARCHAR(300),
     Pinterest			VARCHAR(300),
-    FOREIGN KEY (Userid) REFERENCES UserProfile(Userid),
-    FOREIGN KEY (Facebook) REFERENCES Facebook(FBLINK),
-    FOREIGN KEY (Twitter) REFERENCES Twitter(twitterlink),
-    FOREIGN KEY (Instagram) REFERENCES Instagram(instalink),
-    FOREIGN KEY (Linkedin) REFERENCES Linkedin(linkedinlink),
-    FOREIGN KEY (Github) REFERENCES Github(githublink),
-    FOREIGN KEY (Youtube) REFERENCES Youtube(ytlink),
-    FOREIGN KEY (Snapchat) REFERENCES Snapchat(sclink),
-    FOREIGN KEY (Reddit) REFERENCES Reddit(redditlink),
-    FOREIGN KEY (Pinterest) REFERENCES Pinterest(pintlink)
+    FOREIGN KEY (Userid) REFERENCES UserProfile(username)
     );
